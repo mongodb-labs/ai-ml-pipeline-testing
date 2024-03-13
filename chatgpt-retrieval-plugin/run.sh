b9902ced65b28f874cbc87b614310ad2d0d330df
Casey Clements <casey.clements@mongodb.com>
2024-03-13 18:48:23 -0400

    Add environment variables to run pytest

--- chatgpt-retrieval-plugin/run.sh
+++ chatgpt-retrieval-plugin/run.sh
@@ -21,5 +21,16 @@ source $($PYTHON_BINARY -m poetry env info --path)/bin/activate
 # Install requirements, including those for dev/test
 $PYTHON_BINARY -m poetry lock --no-update
 $PYTHON_BINARY -m poetry install --with dev
-# Run Tests
+
+
+# Run tests, setting sensitive variables in Evergreen
+OPENAI_API_KEY=$openai_api_key \
+DATASTORE="mongodb" \
+BEARER_TOKEN="staylowandkeepmoving" \
+MONGODB_URI=$chatgpt_retrieval_plugin_mongodb_uri \
+MONGODB_DATABASE="chatgpt_retrieval_plugin_test_db" \
+MONGODB_COLLECTION="chatgpt_retrieval_plugin_test_vectorstore" \
+MONGODB_INDEX="vector_index" \
+EMBEDDING_MODEL="text-embedding-3-small" \
+EMBEDDING_DIMENSION="1536" \
 $PYTHON_BINARY -m poetry run pytest tests/datastore/providers/mongodb_atlas/
