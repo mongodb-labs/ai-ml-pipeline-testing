91ccc50d9673a5ca63f78ee83800b1ad496acff3
Casey Clements <casey.clements@mongodb.com>
2024-03-15 12:50:00 -0400

    Changed run.sh back to poetry. Also made psycopg2 and psycopg2-binary optional dependencies

--- chatgpt-retrieval-plugin/run.sh
+++ chatgpt-retrieval-plugin/run.sh
@@ -11,19 +11,17 @@ echo ">>>>>>>>>> run.sh <<<<<<<<<<<<<<<"
 PYTHON_BINARY=$(find_python3) # in utils.sh
 $PYTHON_BINARY -c "import sys; print(f'Python version found: {sys.version_info}')"
 
-$PYTHON_BINARY -m pip install -U pip
+# Install Poetry into base python
+$PYTHON_BINARY -m pip install poetry
+# Create a package specific poetry environment
+$PYTHON_BINARY -m poetry env use $PYTHON_BINARY
+# Activate the poetry env
+source $($PYTHON_BINARY -m poetry env info --path)/bin/activate
+# python3 is now that of the project-specific env
+# Install requirements, including those for dev/test
+$PYTHON_BINARY -m poetry lock
+$PYTHON_BINARY -m poetry install --with dev
 
-# Create a venv environment
-$PYTHON_BINARY -m venv venv
-# Activate the env
-source venv/bin/activate
-echo "$(which python)"
-
-# Install dependencies. Pip will find poetry.dependencies.
-python -m pip install poetry
-python -m pip install .
-# But pip will not find group.*.dependencies
-python -m pip install httpx pytest pytest-cov pytest-asyncio
 
 # Run tests, setting sensitive variables in Evergreen
 OPENAI_API_KEY=$openai_api_key \
@@ -35,4 +33,4 @@ MONGODB_COLLECTION="chatgpt_retrieval_plugin_test_vectorstore" \
 MONGODB_INDEX="vector_index" \
 EMBEDDING_MODEL="text-embedding-3-small" \
 EMBEDDING_DIMENSION="1536" \
-python -m pytest -v tests/datastore/providers/mongodb_atlas/
+$PYTHON_BINARY -m poetry run pytest -v tests/datastore/providers/mongodb_atlas/
