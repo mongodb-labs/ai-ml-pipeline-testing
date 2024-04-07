dc205bf10ca91fa9909c27896c530ae7e91696ba
Casey Clements <casey.clements@mongodb.com>
2024-04-07 15:27:00 -0400

    Cleanup of run.sh for semantic-kernel-python. Adds .env file.

--- semantic-kernel-python/run.sh
+++ semantic-kernel-python/run.sh
@@ -4,19 +4,28 @@ set -x
 
 . $workdir/src/.evergreen/utils.sh
 
-CONN_STRING=$($atlas deployments connect $DIR --connectWith connectionString)
+CONN_STRING=$(atlas deployments connect $DIR --connectWith connectionString)
 PYTHON_BINARY=$(find_python3)
+
 
 # WORKING_DIR = src/semantic-kernel-python/semantic-kernel
 cd python
 
-$PYTHON_BINARY -m venv .
-source ./bin/activate
 
-pip install poetry
-pip install pytest
-pip install grpcio
-poetry install
+# Install Poetry into base python
+$PYTHON_BINARY -m pip install -U pip poetry
+# Create a package specific poetry environment
+$PYTHON_BINARY -m poetry env use $PYTHON_BINARY
+# Activate the poetry env, which itself does not include poetry
+source $($PYTHON_BINARY -m poetry env info --path)/bin/activate
+# Recreate the poetry lock file
+$PYTHON_BINARY -m poetry lock --no-update
+# Install from pyproject.toml into package specific environment
+$PYTHON_BINARY -m poetry install --with dev
+
+# Create .env file
+python -c "from dotenv import set_key; set_key('.env', 'OPENAI_API_KEY', '$openai_api_key')"
+python -c "from dotenv import set_key; set_key('.env', 'MONGODB_ATLAS_CONNECTION_STRING', '$CONN_STRING')"
 
 # Workaround to test vector search first
 OPENAI_API_KEY=$openai_api_key \
