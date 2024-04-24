9dc6a7ef4fb8abf870fd95aca20736133800140b
Casey Clements <casey.clements@mongodb.com>
2024-04-24 17:12:01 -0400

    Updated run.sh
    Signed-off-by: Casey Clements <casey.clements@mongodb.com>

--- docarray/run.sh
+++ docarray/run.sh
@@ -9,25 +9,18 @@ set -x
 PYTHON_BINARY=$(find_python3)
 $PYTHON_BINARY -c "import sys; print(f'Python version found: {sys.version_info}')"
 
-# Install Poetry into base python
-$PYTHON_BINARY -m pip install -U pip poetry
-# Create a package specific poetry environment
-$PYTHON_BINARY -m poetry env use $PYTHON_BINARY
-# Activate the poetry env, which itself does not include poetry
-source $($PYTHON_BINARY -m poetry env info --path)/bin/activate
+# Create and activate an isolated python venv environment
+$PYTHON_BINARY -m venv venv
+source venv/bin/activate
+# Install Poetry
+python -m pip install -U pip poetry
 # Recreate the poetry lock file
-$PYTHON_BINARY -m poetry lock --no-update
+python -m poetry lock --no-update
 # Install from pyproject.toml into package specific environment
-$PYTHON_BINARY -m poetry install --with dev
+python -m poetry install --with dev --extras mongo
+
 
 # Run tests. Sensitive variables in Evergreen come from Evergeen project: ai-ml-pipeline-testing/
-# TODO: UPDATE VARIABLES
-OPENAI_API_KEY=$openai_api_key \
-DATASTORE="mongodb" \
 MONGODB_URI=$docarray_mongodb_uri \
 MONGODB_DATABASE="docarray_test_db" \
-MONGODB_COLLECTION="docarray_test_vectorstore" \
-MONGODB_INDEX="vector_index" \
-EMBEDDING_MODEL="text-embedding-3-small" \
-EMBEDDING_DIMENSION="1536" \
-$PYTHON_BINARY -m poetry run pytest -v tests/index/mongo_atlas
+python -m poetry run pytest -v tests/index/mongo_atlas
