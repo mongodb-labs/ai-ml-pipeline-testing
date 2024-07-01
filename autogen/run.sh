52db18d9e98e7c7a1f3bf4ed7f4b8049b61b67f8
Casey Clements <caseyclements@users.noreply.github.com>
2024-07-01 17:27:25 +0100

    PYTHON-4506 Add autogen (#28)
    * PYTHON-4506 Add Autogen pointing to pull-request
    
    * typo in run.sh
    
    * PYTHON-4506 Had to invoke pytest as a module for some reason.

---
+++ autogen/run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#  Sets up a virtual environment
+#  Runs the mongodb tests of the upstream repo
+# NOTE: In this instance, the tests *programmtically* create_vector_search_index
+
+set -x
+
+. $workdir/src/.evergreen/utils.sh
+PYTHON_BINARY=$(find_python3)
+$PYTHON_BINARY -c "import sys; print(f'Python version found: {sys.version_info}')"
+
+# Create and activate an isolated python venv environment
+$PYTHON_BINARY -m venv venv
+source venv/bin/activate
+#  Install autogen with extras
+$PYTHON_BINARY -m pip install .[test,"retrievechat-mongodb"]
+
+
+# Run tests. Sensitive variables in Evergreen come from Evergreen project: ai-ml-pipeline-testing/
+MONGODB_URI=$autogen_mongodb_uri \
+MONGODB_DATABASE="autogen_test_db" \
+$PYTHON_BINARY -m pytest -v test/agentchat/contrib/vectordb/test_mongodb.py
