b36a02e624c9f44e7f89c6c03cc8d5a6f637113d
Casey Clements <casey.clements@mongodb.com>
2024-08-05 16:15:32 -0400

    PYTHON-4444 Updated chatgpt run.sh

--- chatgpt-retrieval-plugin/run.sh
+++ chatgpt-retrieval-plugin/run.sh
@@ -9,16 +9,16 @@ set -x
 PYTHON_BINARY=$(find_python3)
 $PYTHON_BINARY -c "import sys; print(f'Python version found: {sys.version_info}')"
 
-# Install Poetry into base python
-$PYTHON_BINARY -m pip install -U pip poetry
-# Create a package specific poetry environment
-$PYTHON_BINARY -m poetry env use $PYTHON_BINARY
-# Activate the poetry env, which itself does not include poetry
-source $($PYTHON_BINARY -m poetry env info --path)/bin/activate
+# Create and activate an isolated python venv environment
+$PYTHON_BINARY -m venv venv
+source venv/bin/activate
+
+python -m pip install -U pip poetry
+
 # Recreate the poetry lock file
-$PYTHON_BINARY -m poetry lock --no-update
+python -m poetry lock --no-update
 # Install from pyproject.toml into package specific environment
-$PYTHON_BINARY -m poetry install --with dev
+python -m poetry install --with dev
 
 # Run tests. Sensitive variables in Evergreen come from Evergeen project: ai-ml-pipeline-testing/
 OPENAI_API_KEY=$openai_api_key \
@@ -30,4 +30,4 @@ MONGODB_COLLECTION="chatgpt_retrieval_plugin_test_vectorstore" \
 MONGODB_INDEX="vector_index" \
 EMBEDDING_MODEL="text-embedding-3-small" \
 EMBEDDING_DIMENSION="1536" \
-$PYTHON_BINARY -m poetry run pytest -v tests/datastore/providers/mongodb_atlas/
+python -m poetry run pytest -v tests/datastore/providers/mongodb_atlas/
