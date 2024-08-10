74bda0b42760ef12a31c77685d221bbf6d17d268
Casey Clements <casey.clements@mongodb.com>
2024-08-10 11:54:53 -0400

    Cosmetic changes to langchain run.sh

--- langchain-python/run.sh
+++ langchain-python/run.sh
@@ -8,16 +8,19 @@ set -x
 PYTHON_BINARY=$(find_python3)
 
 cd libs/partners/mongodb
-
+# Create and activate an isolated python venv environment
 $PYTHON_BINARY -m venv venv
 source venv/bin/activate
-
+# Install poetry
+python -m pip install -U pip poetry
+# Recreate the poetry lock file
+python -m poetry lock --no-update
+# Install from pyproject.toml into package specific environment
 python -m poetry install --with lint --with typing --with codespell --with dev --with test --with test_integration
-
+# Export env vars
 export MONGODB_ATLAS_URI=$($atlas deployments connect $DIR --connectWith connectionString)
-
-make test
-
 export OPENAI_API_KEY=$openai_api_key
-
- make integration_test
+# Run unit tests
+make test
+# Run integration tests
+make integration_test
