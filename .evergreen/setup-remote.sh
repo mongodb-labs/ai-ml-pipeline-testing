0cd1e0c9e4b891be02a07f3684de9e9a601c0a98
Steven Silvester <steve.silvester@mongodb.com>
2024-11-20 12:01:30 -0600

    try old remote urls

--- .evergreen/setup-remote.sh
+++ .evergreen/setup-remote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 
-source secrets-export.sh
+# source secrets-export.sh
 
 if [ -z "${DIR:-}" ]; then
     echo "Must give a target dir!"
@@ -11,25 +11,25 @@ fi
 # Get the correct remote URI.
 case $DIR in
     llama-index-python-kvstore)
-        MONGODB_URI=$DOCARRAY_MONGODB_URI
+        MONGODB_URI=$llama_index_mongodb_uri
     ;;
     semantic-kernel-python)
-        MONGODB_URI=$DOCARRAY_MONGODB_URI
+        MONGODB_URI=$semantic_kernel_mongodb_uri
     ;;
     semantic-kernel-csharp)
-        MONGODB_URI=$DOCARRAY_MONGODB_URI
+        MONGODB_URI=$semantic_kernel_mongodb_uri
     ;;
     langchain-python)
-        MONGODB_URI=$DOCARRAY_MONGODB_URI
+        MONGODB_URI=$docarray_mongodb_uri
     ;;
     chatgpt-retrieval-plugin)
-        MONGODB_URI=$DOCARRAY_MONGODB_URI
+        MONGODB_URI=$chatgpt_retrieval_plugin_mongodb_uri
     ;;
     llama-index-python-vectorstore)
-        MONGODB_URI=$DOCARRAY_MONGODB_URI
+        MONGODB_URI=$llama_index_mongodb_uri
     ;;
     docarray)
-        MONGODB_URI=$DOCARRAY_MONGODB_URI
+        MONGODB_URI=$docarray_mongodb_uri
     ;;
     *)
         echo "Missing config in fetch-secrets.sh for DIR: $DIR"
@@ -38,5 +38,5 @@ case $DIR in
 esac
 
 # Create the env file
-echo "export OPENAI_API_KEY=$OPENAI_API_KEY" >> env.sh
+echo "export OPENAI_API_KEY=$openai_api_key" >> env.sh
 echo "export MONGODB_URI=$MONGODB_URI" >> env.sh
