efba00eb61bd711cf3f649e0ebc11150a278adcc
Steven Silvester <steve.silvester@mongodb.com>
2024-11-20 12:16:35 -0600

    try again with new cluster

--- .evergreen/setup-remote.sh
+++ .evergreen/setup-remote.sh
@@ -11,25 +11,25 @@ fi
 # Get the correct remote URI.
 case $DIR in
     llama-index-python-kvstore)
-        MONGODB_URI=$llama_index_mongodb_uri
+        MONGODB_URI=$LLAMA_INDEX_MONGODB_URI
     ;;
     semantic-kernel-python)
-        MONGODB_URI=$semantic_kernel_mongodb_uri
+        MONGODB_URI=$SEMANTIC_KERNEL_MONGODB_URI
     ;;
     semantic-kernel-csharp)
-        MONGODB_URI=$semantic_kernel_mongodb_uri
+        MONGODB_URI=$SEMANTIC_KERNEL_MONGODB_URI
     ;;
     langchain-python)
-        MONGODB_URI=$docarray_mongodb_uri
+        MONGODB_URI=$LANGCHAIN_MONGODB_URI
     ;;
     chatgpt-retrieval-plugin)
-        MONGODB_URI=$chatgpt_retrieval_plugin_mongodb_uri
+        MONGODB_URI=$CHATGPT_RETRIEVAL_PLUGIN_MONGODB_URI
     ;;
     llama-index-python-vectorstore)
-        MONGODB_URI=$llama_index_mongodb_uri
+        MONGODB_URI=$LLAMA_INDEX_MONGODB_URI
     ;;
     docarray)
-        MONGODB_URI=$docarray_mongodb_uri
+        MONGODB_URI=$DOCARRAY_MONGODB_URI
     ;;
     *)
         echo "Missing config in fetch-secrets.sh for DIR: $DIR"
@@ -40,6 +40,3 @@ esac
 # Create the env file
 echo "export OPENAI_API_KEY=$openai_api_key" >> env.sh
 echo "export MONGODB_URI=$MONGODB_URI" >> env.sh
-
-echo "set MONGODB_URI=$MONGODB_URI"
-exit 1
