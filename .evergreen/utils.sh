a0b3400910bcd6292b03ce938bf97fbe061b42e8
Steven Silvester <steve.silvester@mongodb.com>
2025-04-07 09:14:14 -0500

    INTPYTHON-573 Use drivers-tools to run the local atlas server

--- .evergreen/utils.sh
+++ .evergreen/utils.sh
@@ -52,64 +52,17 @@ is_python_310() {
 }
 
 
-retry() {
-    for i in 1 2 4 8 16; do
-        { "$@" && return 0; } || sleep $i
-    done
-    return 1
-}
-
 
 # start mongodb-atlas-local container, because of a bug in podman we have to define the healtcheck ourselves (is the same as in the image)
 # stores the connection string in .local_atlas_uri file
 setup_local_atlas() {
-    echo "Starting the container"
-
-    IMAGE=mongodb/mongodb-atlas-local:latest
-    retry podman pull $IMAGE
-
-    podman kill mongodb_atlas_local || true
-
-    CONTAINER_ID=$(podman run --rm -d -e DO_NOT_TRACK=1 -P --health-cmd "/usr/local/bin/runner healthcheck" $IMAGE)
-    podman rename $CONTAINER_ID mongodb_atlas_local || true
-
-    echo "waiting for container to become healthy..."
-    function wait() {
-    CONTAINER_ID=$1
-
-    echo "waiting for container to become healthy..."
-    podman healthcheck run "$CONTAINER_ID"
-    for _ in $(seq 600); do
-        STATE=$(podman inspect -f '{{ .State.Health.Status }}' "$CONTAINER_ID")
-
-        case $STATE in
-            healthy)
-            echo "container is healthy"
-            return 0
-            ;;
-            unhealthy)
-            echo "container is unhealthy"
-            podman logs "$CONTAINER_ID"
-            stop
-            exit 1
-            ;;
-            *)
-            echo "Unrecognized state $STATE"
-            sleep 1
-        esac
-    done
-
-    echo "container did not get healthy within 120 seconds, quitting"
-    podman logs mongodb_atlas_local
-    stop
-    exit 2
-    }
-
-    wait "$CONTAINER_ID"
-    EXPOSED_PORT=$(podman inspect --format='{{ (index (index .NetworkSettings.Ports "27017/tcp") 0).HostPort }}' "$CONTAINER_ID")
-    export CONN_STRING="mongodb://127.0.0.1:$EXPOSED_PORT/?directConnection=true"
     SCRIPT_DIR=$(realpath "$(dirname ${BASH_SOURCE[0]})")
-    echo "CONN_STRING=mongodb://127.0.0.1:$EXPOSED_PORT/?directConnection=true" > $SCRIPT_DIR/.local_atlas_uri
+    # Ensure drivers-evergeen-tools checkout.
+    pushd $SCRIPT_DIR/..
+    git clone https://github.com/mongodb-labs/drivers-evergreen-tools || true
+    . drivers-evergreen-tools/.evergreen/run-orchestration.sh --local-atlas
+    popd
+    echo "CONN_STRING=mongodb://127.0.0.1:27017/?directConnection=true" > $SCRIPT_DIR/.local_atlas_uri
 }
 
 fetch_local_atlas_uri() {
