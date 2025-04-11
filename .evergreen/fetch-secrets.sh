2400b575e8a5687ebc3a127270d71b6201b45bc2
Steven Silvester <steve.silvester@mongodb.com>
2025-04-11 14:08:30 -0500

    INTPYTHON-573 Use drivers-tools to run the local atlas server (#63)

--- .evergreen/fetch-secrets.sh
+++ .evergreen/fetch-secrets.sh
@@ -3,7 +3,7 @@
 set -eu
 
 # Clone drivers-evergeen-tools.
-git clone https://github.com/mongodb-labs/drivers-evergreen-tools || true
+git clone https://github.com/mongodb-labs/drivers-evergreen-tools
 
 # Get the secrets for drivers/ai-ml-pipeline-testing.
 . drivers-evergreen-tools/.evergreen/secrets_handling/setup-secrets.sh drivers/ai-ml-pipeline-testing
