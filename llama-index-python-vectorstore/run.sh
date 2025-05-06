e1e497a7333920fc7ed5759a935a60d673f64d93
Steven Silvester <steve.silvester@mongodb.com>
2025-05-06 10:20:06 -0500

    switch to uv

--- llama-index-python-vectorstore/run.sh
+++ llama-index-python-vectorstore/run.sh
@@ -16,24 +16,17 @@ $PYTHON_BINARY -c "import sys; print(f'Python version found: {sys.version_info}'
 # shellcheck disable=SC2164
 cd llama-index-integrations/vector_stores/llama-index-vector-stores-mongodb
 
-# Install Poetry
-$PYTHON_BINARY -m venv .venv
-. .venv/bin/activate
-PYTHON_BINARY=$(which python)
-$PYTHON_BINARY -m pip install -U pip poetry
-# Create a package specific poetry environment
-$PYTHON_BINARY -m poetry env use $PYTHON_BINARY
-# Activate the poetry env, which itself does not include poetry
-. "$($PYTHON_BINARY -m poetry env info --path)/bin/activate"
-# Recreate the poetry lock file
-$PYTHON_BINARY -m poetry lock
-# Install from pyproject.toml into package specific environment
-$PYTHON_BINARY -m poetry install --with dev
+# Install uv.
+$PYTHON_BINARY -m venv venv_pipeline
+source venv_pipeline/bin/activate
+pip install -U pip
+pip install uv
 
 # Run tests.
+UV_PYTHON=$PYTHON_BINARY \
 MONGODB_URI="$MONGODB_URI" \
 OPENAI_API_KEY="$OPENAI_API_KEY" \
 MONGODB_DATABASE="llama_index_test_db" \
 MONGODB_COLLECTION="llama_index_test_vectorstore" \
 MONGODB_INDEX="vector_index" \
-$PYTHON_BINARY -m poetry run pytest -v tests
+uv run pytest -v tests
