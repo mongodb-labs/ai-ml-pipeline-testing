c77465e7971b26c77ddaef070b5b910aaf6182d0
bailey <bailey.pearson@mongodb.com>
2025-05-09 11:58:46 -0600

    tests pass

--- langchain-js/run.sh
+++ langchain-js/run.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-echo "Working!"
+git branch
+git switch use-local-atlas
+git log -n 1
+
+if [[ -f "env.sh" ]]; then
+    source env.sh
+fi
+
+# setup node, npm and yarn
+export PATH=/opt/devtools/node22/bin:$PATH
+export npm_config_prefix=$(pwd)
+export PATH=$(pwd)/bin:$PATH
+npm install --global yarn
+
+bash ../../.evergreen/fetch-secrets.sh
+source secrets-export.sh
+
+cd libs/langchain-mongodb
+
+yarn install
+yarn build
+
+yarn add --dev jest-junit
+export JEST_JUNIT_OUTPUT_NAME=results.xml
+
+yarn test:int --reporters=default --reporters=jest-junit
