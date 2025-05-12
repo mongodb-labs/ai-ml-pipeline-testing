e8cafb6878daea067401872efee346faed9a3c98
Steven Silvester <steve.silvester@mongodb.com>
2025-05-12 07:52:51 -0500

    fix teardown

--- .evergreen/teardown.sh
+++ .evergreen/teardown.sh
@@ -2,9 +2,9 @@
 
 set -eu
 
-if [ -n "${REPO_ORG_OVERRIDE}" ]; then
-  echo "REPO_ORG=$REPO_ORG_OVERRIDE"
+if [ -n "${REPO_ORG:-}" ]; then
+  echo "REPO_ORG=$REPO_ORG"
 fi
-if [ -n "${REPO_BRANCH_OVERRIDE}" ]; then
-  echo "REPO_BRANCH=$REPO_BRANCH_OVERRIDE"
+if [ -n "${REPO_BRANCH:-}" ]; then
+  echo "REPO_BRANCH=$REPO_BRANCH"
 fi
