813316878c349af4c22aa5fa3198c709ef7335d3
Steven Silvester <steve.silvester@mongodb.com>
2025-05-12 08:19:14 -0500

    no overrides

--- .evergreen/teardown.sh
+++ .evergreen/teardown.sh
@@ -2,9 +2,16 @@
 
 set -eu
 
+OVERRIDES=
 if [ -n "${REPO_ORG:-}" ]; then
   echo "REPO_ORG=$REPO_ORG"
+  OVERRIDES=1
 fi
 if [ -n "${REPO_BRANCH:-}" ]; then
   echo "REPO_BRANCH=$REPO_BRANCH"
+  OVERRIDES=1
+fi
+
+if [ -z "${OVERRIDES}" ]; then
+  echo "No overrides"
 fi
