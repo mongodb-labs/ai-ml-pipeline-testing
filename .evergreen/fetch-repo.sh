b03bc152800466b621637f11b05953d02757cb8d
Steven Silvester <steve.silvester@mongodb.com>
2025-05-11 21:32:58 -0500

    fix org override

--- .evergreen/fetch-repo.sh
+++ .evergreen/fetch-repo.sh
@@ -19,7 +19,7 @@ source config.env
 set +a
 
 if [ -n "${REPO_ORG_OVERRIDE}" ]; then
-  REPO_ORG="${REPO_BRANCH_OVERRIDE}"
+  REPO_ORG="${REPO_ORG_OVERRIDE}"
 fi
 if [ -n "${REPO_BRANCH_OVERRIDE}" ]; then
   REPO_BRANCH="${REPO_BRANCH_OVERRIDE}"
