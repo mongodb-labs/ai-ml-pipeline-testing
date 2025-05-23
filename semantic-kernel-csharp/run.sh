eff461d1cf4598426fc1c2c06cf7f7c0cb3d722a
adelinowona <adelinowona@gmail.com>
2025-05-23 17:01:53 -0400

    remove unnecessary atlas setup for SK integration testing

--- semantic-kernel-csharp/run.sh
+++ semantic-kernel-csharp/run.sh
@@ -2,13 +2,6 @@
 
 set -eu
 
-# Get the MONGODB_URI.
-SCRIPT_DIR=$(realpath "$(dirname ${BASH_SOURCE[0]})")
-ROOT_DIR=$(dirname $SCRIPT_DIR)
-. $ROOT_DIR/env.sh
-
-. $ROOT_DIR/.evergreen/utils.sh
-
 # WORKING_DIR = $ROOT_DIR/semantic-kernel-csharp/semantic-kernel
 
 # Install .NET
