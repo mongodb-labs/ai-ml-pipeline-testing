d5108786b9b7a12b99ee6ee534dfe3970dd9b660
Preston Vasquez <prestonvasquez@icloud.com>
2025-07-01 10:59:19 -0600

    Add punctuation

--- langchaingo-golang/run.sh
+++ langchaingo-golang/run.sh
@@ -10,6 +10,6 @@ export PATH="$PATH:/opt/golang/$GO_VERSION/bin"
 export GOROOT="/opt/golang/$GO_VERSION"
 
 # TODO(GODRIVER-3606): Update expected error in LangChainGo test for
-# non-tokenized filter
+# non-tokenized filter.
 go test -v -failfast -race -shuffle=on \
   -skip "TestStore_SimilaritySearch_NonExactQuery/with_non-tokenized_filter"
