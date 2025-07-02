25a0452dc0a4ad5615ddb7fce86840a1fde4c0fa
Steven Silvester <steve.silvester@mongodb.com>
2025-07-02 15:12:53 -0500

    debug semantic kernel

--- semantic-kernel-python/run.sh
+++ semantic-kernel-python/run.sh
@@ -34,7 +34,7 @@ OPENAI_API_KEY="" \
     AZURE_OPENAI_API_KEY="" \
     MONGODB_ATLAS_CONNECTION_STRING=$MONGODB_URI \
     Python_Integration_Tests=1 \
-    uv run pytest -v test_mongodb_atlas_memory_store.py -k test_collection_knn
+    uv run pytest -rav test_mongodb_atlas_memory_store.py -k test_collection_knn
 
 # shellcheck disable=SC2154
 OPENAI_API_KEY="" \
@@ -44,4 +44,4 @@ OPENAI_API_KEY="" \
     AZURE_OPENAI_API_KEY="" \
     MONGODB_ATLAS_CONNECTION_STRING=$MONGODB_URI \
     Python_Integration_Tests=1 \
-    uv run pytest -v test_mongodb_atlas_memory_store.py
+    uv run pytest -rav test_mongodb_atlas_memory_store.py
