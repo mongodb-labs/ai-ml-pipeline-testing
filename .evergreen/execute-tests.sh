91ac206d487f03752fa0def82300d2575c657e66
Steven Silvester <steve.silvester@mongodb.com>
2025-07-02 18:27:10 -0500

    cleanup

--- .evergreen/execute-tests.sh
+++ .evergreen/execute-tests.sh
@@ -5,9 +5,6 @@ set -eu
 SCRIPT_DIR=$(realpath "$(dirname ${BASH_SOURCE[0]})")
 ROOT_DIR=$(dirname $SCRIPT_DIR)
 
-# Source the secrets.
-source $ROOT_DIR/secrets-export.sh
-
 # Source the configuration.
 cd ${ROOT_DIR}/${DIR}
 set -a
--- .evergreen/setup-remote.sh
+++ .evergreen/setup-remote.sh
@@ -51,8 +51,16 @@ case $DIR in
 esac
 export MONGODB_URI
 
+# Get the secrets.
+source secrets-export.sh
+
 # Create the env file
-echo "export MONGODB_URI=$MONGODB_URI" > env.sh
+echo "export VOYAGEAI_S3_BUCKET=$VOYAGEAI_S3_BUCKET" >> env.sh
+echo "export AZURE_OPENAI_ENDPOINT=$AZURE_OPENAI_ENDPOINT" >> env.sh
+echo "export AZURE_OPENAI_API_KEY=$AZURE_OPENAI_API_KEY" >> env.sh
+echo "export OPENAI_API_VERSION=$OPENAI_API_VERSION" >> env.sh
+echo "export MONGODB_URI=$CONN_STRING" >> env.sh
+echo "export VOYAGEAI_API_KEY=$VOYAGEAI_API_KEY" >> env.sh
 
 # Ensure the remote database is populated.
 . .evergreen/utils.sh
