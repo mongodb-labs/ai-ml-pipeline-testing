c9fbf41593d437988cdda0078b546a411e26211d
Steven Silvester <steve.silvester@mongodb.com>
2025-07-03 12:29:52 -0500

    INTPYTHON-667 Migrate to use Azure OpenAI (#84)

--- semantic-kernel-python/run.sh
+++ semantic-kernel-python/run.sh
@@ -27,21 +27,21 @@ make install-pre-commit
 cp $SCRIPT_DIR/test_mongodb_atlas_memory_store.py .
 
 # shellcheck disable=SC2154
-OPENAI_API_KEY="$OPENAI_API_KEY" \
+OPENAI_API_KEY="" \
     OPENAI_ORG_ID="" \
     AZURE_OPENAI_DEPLOYMENT_NAME="" \
     AZURE_OPENAI_ENDPOINT="" \
     AZURE_OPENAI_API_KEY="" \
     MONGODB_ATLAS_CONNECTION_STRING=$MONGODB_URI \
     Python_Integration_Tests=1 \
-    uv run pytest test_mongodb_atlas_memory_store.py -k test_collection_knn
+    uv run pytest -rav test_mongodb_atlas_memory_store.py -k test_collection_knn
 
 # shellcheck disable=SC2154
-OPENAI_API_KEY="$OPENAI_API_KEY" \
+OPENAI_API_KEY="" \
     OPENAI_ORG_ID="" \
     AZURE_OPENAI_DEPLOYMENT_NAME="" \
     AZURE_OPENAI_ENDPOINT="" \
     AZURE_OPENAI_API_KEY="" \
     MONGODB_ATLAS_CONNECTION_STRING=$MONGODB_URI \
     Python_Integration_Tests=1 \
-    uv run pytest test_mongodb_atlas_memory_store.py
+    uv run pytest -rav test_mongodb_atlas_memory_store.py
