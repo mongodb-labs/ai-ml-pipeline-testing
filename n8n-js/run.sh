7c875fd24215737afbaec087a1ac4ae7e064b822
bailey <bailey.pearson@mongodb.com>
2025-08-28 14:13:39 -0600

    should pass

--- n8n-js/run.sh
+++ n8n-js/run.sh
@@ -1,5 +1,28 @@
 #! /bin/bash
 
-echo "running!"
+set -o errexit
+set -o xtrace
 
-ls
+mkdir npm_global_dir
+export NPM_CONFIG_PREFIX=$(pwd)/npm_global_dir
+export PATH="$NPM_CONFIG_PREFIX/bin:$PATH"
+
+npm i -g pnpm
+
+# use node20
+export PATH="/opt/devtools/node22/bin:/opt/dev/tools/bin:$PATH"
+
+pnpm --version
+
+pnpm install
+pnpm build
+
+# first, run the crud node tests
+cd packages/nodes-base/nodes/MongoDb
+pnpm test $(pwd)
+cd -
+
+# then, run the vector store tests
+cd packages/@n8n/nodes-langchain/nodes/vector_store/VectorStoreMongoDBAtlas/
+pnpm test $(pwd)
+cd -
