867f5f2a0d0b9d9ab11c48d9b902bfc6e2f3f20e
Noah Stapp <noah.stapp@mongodb.com>
2025-12-02 10:28:16 -0500

    debugging

--- .evergreen/execute-tests.sh
+++ .evergreen/execute-tests.sh
@@ -16,11 +16,15 @@ cd ${REPO_NAME}
 MAX_ATTEMPTS=3
 ATTEMPT=1
 EXIT_CODE=0
+set +e
 
 while [ $ATTEMPT -le $MAX_ATTEMPTS ]; do  
+  echo "Running test attempt $ATTEMPT"
   bash ${ROOT_DIR}/${DIR}/run.sh
     
   EXIT_CODE=$?
+
+  echo "Test attempt $ATTEMPT had exit code $EXIT_CODE"
 
   if [ $EXIT_CODE -eq 0 ]; then  
     break  
