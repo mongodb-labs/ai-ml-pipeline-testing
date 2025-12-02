8cefd06916b0ee94253544134a2904ed520db35b
Noah Stapp <noah.stapp@mongodb.com>
2025-12-02 09:00:41 -0500

    PYTHON-5388 - Add two automatic retries

--- .evergreen/execute-tests.sh
+++ .evergreen/execute-tests.sh
@@ -13,4 +13,23 @@ set +a
 
 cd ${REPO_NAME}
 
-bash ${ROOT_DIR}/${DIR}/run.sh
+MAX_ATTEMPTS=3
+ATTEMPT=1
+
+while [ $ATTEMPT -le $MAX_ATTEMPTS ]; do  
+  bash ${ROOT_DIR}/${DIR}/run.sh
+    
+  EXIT_CODE=$?
+
+  if [ $EXIT_CODE -eq 0 ]; then  
+    break  
+  else  
+    echo "Tests failed with exit code $EXIT_CODE on attempt $ATTEMPT of $MAX_ATTEMPTS..."  
+    ((ATTEMPT++))  
+  fi  
+done  
+
+if [ $EXIT_CODE -ne 0 ]; then  
+  echo "Tests failed after $MAX_ATTEMPTS attempts."
+fi  
+exit($EXIT_CODE)
