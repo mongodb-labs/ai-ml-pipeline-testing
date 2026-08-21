bf9f54a35f54bf57e167fedbf7a50c8424da2f8e
Casey Clements <casey.clements@mongodb.com>
2026-08-21 14:56:45 -0700

    removed mongodb from langchain-deepagents-vfs dir and task names following convention.

--- .evergreen/setup-remote.sh
+++ .evergreen/setup-remote.sh
@@ -23,7 +23,7 @@ case $DIR in
     semantic-kernel-csharp)
         MONGODB_URI=$SEMANTIC_KERNEL_MONGODB_URI
     ;;
-    langchain-python | langgraph-python | langgraph-store-python)
+    langchain-python | langgraph-python | langgraph-store-python | langchain-deepagents-vfs-python)
         MONGODB_URI=$LANGCHAIN_MONGODB_URI
     ;;
     chatgpt-retrieval-plugin)
